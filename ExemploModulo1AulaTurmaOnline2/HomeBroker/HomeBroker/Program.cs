702711c04d7439f977331dbccbf707eaea2e8cb2
mbcorp <contato@mbcorp.com.br>
2014-04-30 22:55:32 -0300

    Inclusao do Exemplo Modulo 1 Aula Turma Online 2

--- ExemploModulo1AulaTurmaOnline2/HomeBroker/HomeBroker/Program.cs
+++ ExemploModulo1AulaTurmaOnline2/HomeBroker/HomeBroker/Program.cs
@@ -1,4 +1,8 @@
-﻿namespace HomeBroker
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Remoting;
+
+namespace HomeBroker
 {
     class Program
     {
@@ -8,58 +12,118 @@
     }
 
 
-
     public class Acao
     {
-    }
+        public string Codigo { get; set; }
+        public Empresa Empresa { get; private set; }
+        public Cotacao Cotacao { get; private set; }
+        public TipoAcao TipoAcao { get; set; }
 
-    public class Carteira
-    {        
-    }
-
-    public class Corretora
-    {
-    }
-
-    public class Ordem
-    {
-    }
-
-    public class Cliente
-    {
-    }
-
-    public class Pessoa
-    {
-    }
-
-    public class PessoaFisica : Pessoa
-    {
-    }
-
-    public class PessoaJuridica : Pessoa
-    {
-    }
-
-    //Ordem e ações
-    public class Documento
-    {
+        public void ConsultarCotacao()
+        {
+            
+        }
     }
 
     public class Cotacao
     {
+        public double ValorUnitario { get; set; }
     }
-
-
-    public class HomeBroker
-    {
-    }
-
-    public class Bolsa
-    {
-    }
-
-
     
+    public class Empresa
+    {
+        public string Nome { get; set; }
+    }
 
+    public class Carteira
+    {
+        public List<Ordem> Ordems { get; private set; }
+
+        public void AdicionarOrdem(Ordem ordem)
+        {
+            Ordems.Add(ordem);
+        }
+    }
+
+    public class Corretora
+    {
+        public string Nome { get; set; }        
+        public double TaxaDeServico { get; set; }
+
+        public void EnviarOrdem(Ordem ordem)
+        {
+        }
+    }
+
+    public abstract class Ordem
+    {
+        public Acao Acao { get; set; }
+        public DateTime DataOrdem { get; set; }
+        public DateTime Validade { get; set; }
+        public double Valor { get; set; }
+        public double Quantidade { get; set; }
+
+        public abstract bool Cancelar();
+        public abstract bool Executar();
+       
+    }
+
+    public class Compra : Ordem
+    {
+        public override bool Cancelar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Executar()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Venda : Ordem
+    {
+        public double ValorStopLoss { get; set; }
+
+        public override bool Cancelar()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool Executar()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public abstract class Cliente
+    {
+        public Carteira Carteira { get; set; }
+        public TipoInvestidor TipoInvestidor { get; set; }
+    }
+
+    public class ClienteFisica : Cliente
+    {
+        public string CPF { get; set; }
+    }
+
+    public class ClienteJuridica : Cliente
+    {
+        public string CNPJ { get; set; }
+    }
+
+    public enum TipoInvestidor
+    {
+        AltoRisco,
+        Normal
+
+    }
+
+    public enum TipoAcao
+    {
+        ON,
+        PN
+    }
+
+    public class HomeBroker{}
 }
