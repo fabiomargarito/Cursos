910e288e3f0323bc585b05739676bcf68f390da8
mbcorp <contato@mbcorp.com.br>
2014-04-30 21:06:29 -0300

    Inclusao do Exemplo Modulo 1 Aula Turma Online 2

---
+++ ExemploModulo1AulaTurmaOnline2/HomeBroker/HomeBroker/Program.cs
@@ -0,0 +1,65 @@
+﻿namespace HomeBroker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+
+
+    public class Acao
+    {
+    }
+
+    public class Carteira
+    {        
+    }
+
+    public class Corretora
+    {
+    }
+
+    public class Ordem
+    {
+    }
+
+    public class Cliente
+    {
+    }
+
+    public class Pessoa
+    {
+    }
+
+    public class PessoaFisica : Pessoa
+    {
+    }
+
+    public class PessoaJuridica : Pessoa
+    {
+    }
+
+    //Ordem e ações
+    public class Documento
+    {
+    }
+
+    public class Cotacao
+    {
+    }
+
+
+    public class HomeBroker
+    {
+    }
+
+    public class Bolsa
+    {
+    }
+
+
+    
+
+}
