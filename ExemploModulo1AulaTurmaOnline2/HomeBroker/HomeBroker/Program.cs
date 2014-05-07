b6f213a8fa6fa1523ede21e9caba23d64145563f
mbcorp <contato@mbcorp.com.br>
2014-05-07 19:14:35 -0300

    Inclusao do material do segundo modulo

--- ExemploModulo1AulaTurmaOnline2/HomeBroker/HomeBroker/Program.cs
+++ ExemploModulo1AulaTurmaOnline2/HomeBroker/HomeBroker/Program.cs
@@ -1,27 +1,30 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Runtime.Remoting;
+using System.Security.Cryptography.X509Certificates;
 
 namespace HomeBroker
 {
-    class Program
+    //estou aqui!
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
         }
     }
-
-
+    
+    //Arquivo 
     public class Acao
     {
         public string Codigo { get; set; }
         public Empresa Empresa { get; private set; }
         public Cotacao Cotacao { get; private set; }
-        public TipoAcao TipoAcao { get; set; }
+        public Compra.TipoAcao TipoAcao { get; set; }
 
         public void ConsultarCotacao()
         {
-            
+
         }
     }
 
@@ -29,7 +32,7 @@ namespace HomeBroker
     {
         public double ValorUnitario { get; set; }
     }
-    
+
     public class Empresa
     {
         public string Nome { get; set; }
@@ -37,17 +40,154 @@ namespace HomeBroker
 
     public class Carteira
     {
-        public List<Ordem> Ordems { get; private set; }
+        private List<Ordem> Ordems = new List<Ordem>();
 
-        public void AdicionarOrdem(Ordem ordem)
-        {
+        public void AdicionarOrdem(Ordem ordem, Corretora corretora)
+        {              
+            var corretagem = new Corretagem();
+            ordem.Valor += corretagem.CalcularCorretagem(ordem.Valor, corretora);
             Ordems.Add(ordem);
+        }
+
+        public List<Ordem> RetornarOrdems()
+        {
+            return Ordems;
         }
     }
 
+    public class Corretagem
+    {
+        public double CalcularCorretagem(Double valor,Corretora corretora)
+        {                     
+            //calcular
+            if (corretora.Nome == "ITAU")
+                valor =  new CorretagemItau().CalcularCorretagem(valor);    
+            if (corretora.Nome == "XP")
+                valor = new CorretagemXP().CalcularCorretagem(valor);
+            if (corretora.Nome == "SOCOPA")
+                valor = new CorretagemSOCOPA().CalcularCorretagem(valor);
+            if (corretora.Nome == "BANIF")
+                valor = new CorretagemSOCOPA().CalcularCorretagem(valor);
+            if (corretora.Nome == "WintradePadrao")
+            {
+                CorretagemBase corretagem = new CorretagemWintradeInvestidorPadrao();               
+                valor = corretagem.CalcularCorretagem(valor);
+            }
+            if (corretora.Nome == "WintradeQualificado")
+            {
+                CorretagemBase corretagem = new CorretagemWintradeInvestidorQualificado();
+                valor = corretagem.CalcularCorretagem(valor);
+            }
+
+            return valor;
+        }
+    }
+
+
+    public abstract class CorretagemBase
+    {
+        public abstract double CalcularCorretagem(double valor);
+    }
+
+
+    public class CorretagemAgora:CorretagemBase
+    {
+        public override double CalcularCorretagem(double valor)
+        {
+            valor += 13;
+            return valor;
+        }
+    }
+
+
+    public class CorretagemWintradeInvestidorPadrao : CorretagemBase
+    {
+        public int tipoInvestidor;
+
+        public override double CalcularCorretagem(double valor)
+        {
+            if (tipoInvestidor==1)
+                valor += 34;
+            if (tipoInvestidor == 2)
+                valor += 10;
+            return valor;
+        }
+    }
+
+    public class CorretagemWintradeInvestidorQualificado : CorretagemBase
+    {
+        public int tipoInvestidor;
+
+        public override double CalcularCorretagem(double valor)
+        {
+            if (tipoInvestidor == 1)
+                valor += 34;
+            if (tipoInvestidor == 2)
+                valor += 10;
+            return valor;
+        }
+    }
+
+
+    public interface IEmolumento
+    {        
+        double CalcularEmolumento(double valor);
+    }
+
+
+    public interface ICorretagem
+    {
+        double CalcularCorretagem(double valor);        
+    }
+
+    public class CorretagemItau:ICorretagem
+    {
+        public double CalcularCorretagem(double valor)
+        {
+            valor += 10 + valor * 0.03;
+            return valor;
+        }
+
+        
+    }
+
+    public class  CorretagemXP:ICorretagem
+    {
+        public double CalcularCorretagem(double valor)
+        {
+            valor += 14.30;
+            return valor;
+        }
+
+        
+    }
+
+    public class CorretagemSOCOPA : ICorretagem
+    {
+        public double CalcularCorretagem(double valor)
+        {
+            valor += 39;
+            return valor;
+        }
+
+       
+    }
+
+
+    public class CorretagemBANIF : ICorretagem
+    {
+        public double CalcularCorretagem(double valor)
+        {
+            valor += 5;
+            return valor;
+        }
+        
+    }
+
+
     public class Corretora
     {
-        public string Nome { get; set; }        
+        public string Nome { get; set; }
         public double TaxaDeServico { get; set; }
 
         public void EnviarOrdem(Ordem ordem)
@@ -65,7 +205,7 @@ namespace HomeBroker
 
         public abstract bool Cancelar();
         public abstract bool Executar();
-       
+
     }
 
     public class Compra : Ordem
@@ -79,51 +219,55 @@ namespace HomeBroker
         {
             throw new NotImplementedException();
         }
-    }
 
-    public class Venda : Ordem
-    {
-        public double ValorStopLoss { get; set; }
 
-        public override bool Cancelar()
+
+        public class Venda : Ordem
         {
-            throw new NotImplementedException();
+            public double ValorStopLoss { get; set; }
+
+            public override bool Cancelar()
+            {
+                return true;
+            }
+
+            public override bool Executar()
+            {
+                throw new NotImplementedException();
+            }
         }
 
-        public override bool Executar()
+        public abstract class Cliente
         {
-            throw new NotImplementedException();
+            public Carteira Carteira { get; set; }
+            public TipoInvestidor TipoInvestidor { get; set; }
+        }
+
+        public class ClienteFisica : Cliente
+        {
+            public string CPF { get; set; }
+        }
+
+        public class ClienteJuridica : Cliente
+        {
+            public string CNPJ { get; set; }
+        }
+
+        public enum TipoInvestidor
+        {
+            AltoRisco,
+            Normal
+
+        }
+
+        public enum TipoAcao
+        {
+            ON,
+            PN
+        }
+
+        public class HomeBroker
+        {
         }
     }
-
-    public abstract class Cliente
-    {
-        public Carteira Carteira { get; set; }
-        public TipoInvestidor TipoInvestidor { get; set; }
-    }
-
-    public class ClienteFisica : Cliente
-    {
-        public string CPF { get; set; }
-    }
-
-    public class ClienteJuridica : Cliente
-    {
-        public string CNPJ { get; set; }
-    }
-
-    public enum TipoInvestidor
-    {
-        AltoRisco,
-        Normal
-
-    }
-
-    public enum TipoAcao
-    {
-        ON,
-        PN
-    }
-
-    public class HomeBroker{}
 }
