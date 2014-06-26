aeb6e7b73eaaa1bd5e3f374b0a2e9b3e547079e9
Leandro Ribeiro <github@leandroribeiro.com>
2014-06-26 00:08:07 -0300

    Adicionando exemplos utilizados para Turma 3 Online

---
+++ src/Turma3Online/Exemplos/Exemplos.OOP.Heranca.Simples1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Exemplos.OOP.Heranca.Simples1.Model;
+
+namespace Exemplos.OOP.Heranca.Simples1 {
+    class Program {
+        static void Main(string[] args) {
+
+            var pessoas = new Pessoa[]
+                {
+                    new Empregado() { Nome = "Huguinho" }, 
+                    new Estudante() { Nome = "Zezinho Estudantes" }
+                };
+
+            foreach (var pessoa in pessoas) {
+                Console.Write(pessoa.Nome);
+            }
+
+            System.Console.ReadLine();
+        }
+    }
+}
