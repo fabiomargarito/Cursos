d57b9bb9e108a415e8c2d2c30184c2bbad89a700
Leandro Ribeiro <github@leandroribeiro.com>
2014-06-29 22:40:51 -0300

    Removendo exemplos da turma 3

--- src/Turma3Online/Exemplos/Exemplos.Patterns.Factory/Exemplo1/IDAO.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Exemplos.Patterns.Factory.Exemplo1
-{
-    public interface IDAO {
-        void Inserir(string nome);
-        void Excluir(int id);
-        string ObterNome(int id);
-    }
-}
--- src/Turma3Online/Exemplos/Exemplos.Patterns.Factory/Exemplo1/LogEmTela.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Exemplos.Patterns.Factory.Exemplo1 {
-    class LogEmTela : ILog {
-        public void LogarEmTela(string texto) {
-            Console.WriteLine(texto);
-        }
-    }
-}
