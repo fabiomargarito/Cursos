14b793da80d1c441c97bac6571605867b67cbec1
mbcorp <contato@mbcorp.com.br>
2014-10-14 00:27:14 -0300

    exemplos aula 7

--- InjecaoDeDependencia/ExemploHomeBroker/Acao.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Dominio
-{
-    public class Acao
-    {
-        public Cotacao Cotacao { get; set; }
-        public Empresa Empresa { get; set; }
-        public string Codigo { get; set; }
-    }
-}
