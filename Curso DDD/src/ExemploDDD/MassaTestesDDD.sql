894e4bd5035dce188f8ba10f0783b4d84dab793e
mbcorp <contato@mbcorp.com.br>
2015-01-29 09:01:50 -0200

    Atualizacao Aula 5, SmartUI e Repositorios

--- Curso DDD/src/ExemploDDD/MassaTestesDDD.sql
+++ Curso DDD/src/ExemploDDD/MassaTestesDDD.sql
@@ -1,11 +1,15 @@
 use MBCORPHEALTHDDD 
 GO
 
+insert into tbAtendente values('222','Marcelo')
 INSERT INTO TBMEDICO VALUES ('123','FABIO MARGARITO','SP')
 INSERT INTO TBPLANOSAUDE VALUES('12345','PORTO','TOP')
 insert into TBPACIENTE values ('1234','fabio paciente','fabiomargarito@gmail.com','12345')
 insert into TBTIPOEXAME VALUES('1','1')
-insert into TBLAUDO values ('teste')
-insert into TBCREDENCIAL values ('fabiomargarito@gmail.com','1234')
-insert into TBAGENDAMENTO values ('1234',1, '123')
-insert into TBEXAME values(1,'1',1)
+insert into TBLAUDO values (1,'teste')
+insert into TBCREDENCIAL values (1,'fabiomargarito@gmail.com','1234')
+insert into TBAGENDAMENTO values ('222', 1,'123','1234')
+insert into TBEXAME values(1,1,'1',1)
+
+
+	
