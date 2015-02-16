4e7fcd43dab3a249c3671d499f92e428e9f4efee
mbcorp <contato@mbcorp.com.br>
2015-02-16 19:14:18 -0200

    Exemplo Aula 7 Parte 2

--- Curso DDD/src/ExemploDDD/MBCorpHealthTestTest/TestesIntegradosContextoBooking.cs
+++ Curso DDD/src/ExemploDDD/MBCorpHealthTestTest/TestesIntegradosContextoBooking.cs
@@ -13,35 +13,71 @@ namespace MBCorpHealthTestTest
     [TestClass]
     public class TestesIntegradosContextoBooking
     {
+
+        [TestMethod]
+        public void TestParaTestarOJenkins ()
+    { }
+
         [TestMethod]
         public void ComoUsuarioDoServicoDeAgendamentoQueroAAgendaDisponivelParaUmTipoDeExameEUmCentroDeDiagnostico()
         {
             //Arrange
 
-            HttpClient cliente = null;
-            Uri enderecoDoServico = new Uri("http://servicodebooking.azurewebsites.net");
+            IMeuServicoDeBooking meuServicoDeBooking = new ServicoDeBookingRest();
 
-            cliente = new HttpClient();
-            cliente.BaseAddress = enderecoDoServico;
-            cliente.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            //act
 
-            //Act
-
-            HttpResponseMessage resposta =
-                cliente.GetAsync(string.Format("api/ServicoBooking?CBHPM={0}&codigoCentroDeDiagnostico={1}", 1, 1))
-                    .Result;
-
-
-            List<DateTime> datasDisponiveis = new List<DateTime>();
-            if (resposta.IsSuccessStatusCode)
-            {
-                datasDisponiveis.AddRange(resposta.Content.ReadAsAsync<IEnumerable<DateTime>>().Result);
-            }
-
+            var datasDisponiveis = meuServicoDeBooking.RetornarAgendaDisponivelParaOExameEUnidadeDEDiagnostico("1", "1");
 
             //Assert
             Assert.IsTrue(datasDisponiveis.Count >0);
 
         }
+
+        //target
+        public interface IMeuServicoDeBooking
+        {
+             List<DateTime> RetornarAgendaDisponivelParaOExameEUnidadeDEDiagnostico(string cbhpm,string CodigoDaUnidadeDeDiagnostico);
+        }
+
+
+        //Adapter
+          class ServicoDeBookingWS:IMeuServicoDeBooking
+          {
+              public List<DateTime> RetornarAgendaDisponivelParaOExameEUnidadeDEDiagnostico(string cbhpm, string CodigoDaUnidadeDeDiagnostico)
+              {
+                  throw new NotImplementedException();
+              }
+          }
+
+        //Adapter
+        class ServicoDeBookingRest:IMeuServicoDeBooking
+        {
+            public List<DateTime> RetornarAgendaDisponivelParaOExameEUnidadeDEDiagnostico(string cbhpm, string CodigoDaUnidadeDeDiagnostico)
+            {
+                HttpClient cliente = null;
+                Uri enderecoDoServico = new Uri("http://servicodebooking.azurewebsites.net");
+
+                cliente = new HttpClient();
+                cliente.BaseAddress = enderecoDoServico;
+                cliente.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                
+
+                HttpResponseMessage resposta =
+                    cliente.GetAsync(string.Format("api/ServicoBooking?CBHPM={0}&codigoCentroDeDiagnostico={1}", 1, 1))
+                        .Result;
+
+
+                List<DateTime> datasDisponiveis = new List<DateTime>();
+                if (resposta.IsSuccessStatusCode)
+                {
+                    datasDisponiveis.AddRange(resposta.Content.ReadAsAsync<IEnumerable<DateTime>>().Result);
+                }
+
+                return datasDisponiveis;
+            }
+        }
+
+
     }
 }
