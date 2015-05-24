95706af688e7e1fb2302104fd9463afdd0c60f73
Fabio Margarito Martins de Barros <fabiomargarito@gmail.com>
2015-05-24 12:06:55 -0300

    Atualizacao Curso Fundamentos Sabado Turma 4

---
+++ Curso Fundamentos/src/Turma4Sabado/OO/ClassLibrary1/ClassLibrary1/Pessoa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CadastroBasico
+{
+    class Pessoa
+    {
+        public string Nome { get; set; }
+        public DateTime DataNascimento { get; set; }
+
+        public int CalcularIdade()
+        {
+            return DateTime.Now.Year - DataNascimento.Year;
+        }
+
+    }
+}
