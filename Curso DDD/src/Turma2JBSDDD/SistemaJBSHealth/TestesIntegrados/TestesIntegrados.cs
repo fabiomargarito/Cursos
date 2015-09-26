c9101269efddfef60aeca0487c8f227e21993de8
Fabio Margarito <fabiomargarito@gmail.com>
2015-09-26 12:17:44 -0300

    Atualizacao Testes Integrados 4

--- Curso DDD/src/Turma2JBSDDD/SistemaJBSHealth/TestesIntegrados/TestesIntegrados.cs
+++ Curso DDD/src/Turma2JBSDDD/SistemaJBSHealth/TestesIntegrados/TestesIntegrados.cs
@@ -14,34 +14,34 @@ namespace SistemaJBSHealth.TestesIntegrados
     public class TestesIntegrados
     {
         ///[TestMethod]
-        public void TesteIntegradoComoAtendenteQueroCadastrarUmAgendamento()
-        {
-            //Arrange
-            Agendamento agendamento =
-               (new FabricaDeAgendamento())
-                    .InformarExame("234", "Exame 2", 200)
-                    .InformarMedico("444", "Dr.Joao")
-                    .InformarPaciente("12345", "Fabio Margarito")
-                    .InformarExame("1234", "Exame Sangue Completo", 100)
-                    .Criar();
+        //public void TesteIntegradoComoAtendenteQueroCadastrarUmAgendamento()
+        //{
+        //    //Arrange
+        //    Agendamento agendamento =
+        //       (new FabricaDeAgendamento())
+        //            .InformarExame("234", "Exame 2", 200)
+        //            .InformarMedico("444", "Dr.Joao")
+        //            .InformarPaciente("12345", "Fabio Margarito")
+        //            .InformarExame("1234", "Exame Sangue Completo", 100)
+        //            .Criar();
 
-            bool retorno;
-            ISessionORM sessaoNHibernate = new SessionORM();
-            using (var sessao = NHibernateSessionFactory.Criar().OpenSession())
-            {
-                //((SessionORM)sessaoNHibernate).Sessao = sessao;
-                IServicoDeCadastramentoDeAgendamento servicoDeCadastramentoDeAgendamento = new ServicoDeCadastramentoDeAgendamentoFake(sessaoNHibernate);
-                retorno = servicoDeCadastramentoDeAgendamento.Cadastrar(agendamento);
-                sessao.Flush();
-            }
+        //    bool retorno;
+        //    ISessionORM sessaoNHibernate = new SessionORM();
+        //    using (var sessao = NHibernateSessionFactory.Criar().OpenSession())
+        //    {
+        //        //((SessionORM)sessaoNHibernate).Sessao = sessao;
+        //        IServicoDeCadastramentoDeAgendamento servicoDeCadastramentoDeAgendamento = new ServicoDeCadastramentoDeAgendamentoFake(sessaoNHibernate);
+        //        retorno = servicoDeCadastramentoDeAgendamento.Cadastrar(agendamento);
+        //        sessao.Flush();
+        //    }
 
 
-            //Assert
-            Assert.IsTrue(agendamento.Medico != null);
-            Assert.IsTrue(agendamento.Paciente != null);
-            Assert.IsTrue(((IList)agendamento.Exames).Count > 0);
-            Assert.IsTrue(retorno);
-        }
+        //    //Assert
+        //    Assert.IsTrue(agendamento.Medico != null);
+        //    Assert.IsTrue(agendamento.Paciente != null);
+        //    Assert.IsTrue(((IList)agendamento.Exames).Count > 0);
+        //    Assert.IsTrue(retorno);
+        //}
     }
 }
 
