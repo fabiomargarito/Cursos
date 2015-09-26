d7b9e3fe4e8425ce31e413835a4a802312d16bad
Fabio Margarito <fabiomargarito@gmail.com>
2015-09-26 07:38:05 -0300

    Atualizacao Exemplos

--- Curso DDD/src/Turma2JBSDDD/JBSMedical/Infraestrutura/Repositorios/ConfiguracaoNHibernate.cs
+++ Curso DDD/src/Turma2JBSDDD/JBSMedical/Infraestrutura/Repositorios/ConfiguracaoNHibernate.cs
@@ -14,7 +14,7 @@ namespace JBSMedical.Infraestrutura.Repositorios
                 .Database(                 
                     MsSqlConfiguration.MsSql2012.ConnectionString(
                         conexao => conexao.FromConnectionStringWithKey("JBSTurma2")))
-                .ExposeConfiguration(config => new SchemaExport(config).SetOutputFile(@"C:\Users\Fabio\Dropbox\Treinamentos\ExemplosModulo2\Curso DDD\src\Turma2JBSDDD\Script\jbsturma2.sql").Execute(true, true, false))
+                .ExposeConfiguration(config => new SchemaExport(config).SetOutputFile(@"jbsturma2.sql").Execute(true, true, false))
                 .BuildSessionFactory();
 
             return sessionFactory;
